95094aeec3794292aa1481076655d04be9bfd1d1
Arman-Kabir <armankabir8@gmail.com>
2022-09-13 19:46:51 +0600

    routes started

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ import express from 'express';
 // const express = require('express');
 import bodyParser from 'body-parser';
 
+import usersRoutes from './routes/users.js';
+
 const app = express();
 const PORT = 5000;
 
 app.use(bodyParser.json());
 
-app.get('/', (req,res) => {
-    console.log('[TEST]!');
+app.use('/users',usersRoutes);
 
-    res.send('Hello from homepage.');
-});
 
-app.listen(PORT, () => {
-    console.log(`Server Running on port: http://localhost:${PORT}`);
-})
+
+
+app.get('/', (req,res) =>   res.send('Hello from homepage.'));
+
+app.listen(PORT, () => console.log(`Server Running on port: http://localhost:${PORT}`));
